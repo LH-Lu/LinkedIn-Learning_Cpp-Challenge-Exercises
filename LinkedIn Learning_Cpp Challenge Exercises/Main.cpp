0ae4631336f1e45e43bbe661252c4835e05732cc
L Lv <luleheng@live.com>
2024-06-27 16:39:30 +0800

    Set up Header Files and Main Function

--- LinkedIn Learning_Cpp Challenge Exercises/Main.cpp
+++ LinkedIn Learning_Cpp Challenge Exercises/Main.cpp
@@ -1,2 +1,24 @@
-// This is the main folder
+// This is the main function --> access to individual exercises
 
+#include"CommonHeader.h"
+#include"ExHeader.h"
+
+
+int main() {
+
+	int ExNumber = -1;
+
+	std::cout << "Please enter Exercise Number: ";
+	std::cin >> ExNumber;
+	std::cout << std::endl;
+
+
+	switch (ExNumber) {
+	case 1:
+		break;
+	}
+
+	std::cout << "*** PROGRAM ENDING ***" << std::endl;
+
+	return 0;
+}
